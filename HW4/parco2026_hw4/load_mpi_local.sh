07071fd7e1ceeff4aa5f477707d8a612456c882a
vinceztwang <vinceztwang@tencent.com>
2026-06-05 16:15:23 +0800

    minor fix

--- HW4/parco2026_hw4/load_mpi_local.sh
+++ HW4/parco2026_hw4/load_mpi_local.sh
@@ -1,3 +1,11 @@
 export OMPI_ALLOW_RUN_AS_ROOT=1
 export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
+
+# Silence PSM3 / OFI libfabric NIC probe warnings on hosts without a usable
+# high-perf fabric. Force Open MPI onto the safe shared-memory + TCP path.
+export OMPI_MCA_pml=ob1
+export OMPI_MCA_btl=self,vader,tcp
+export OMPI_MCA_mtl=^ofi,psm3
+export PSM3_DEVICES=self,shm
+
 module load mpi/openmpi-x86_64
